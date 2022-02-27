8cc96aabcb8bdc37b2c2eeb781032147bd60f5ec
Patrick Lin <apocalypsecalculator@gmail.com>
2022-02-26 20:49:21 -0500

    counter and graceful exit

--- index.js
+++ index.js
@@ -1,32 +1,58 @@
 const childProcess = require('child_process');
 const chalk = require('chalk');
 
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
-const argv = yargs(hideBin(process.argv)).argv
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
 
-let workerCount = 1;
+let workerCount = 10;
+let counterMap = new Map();
 
-if(argv.workers && !isNaN(argv.workers) && parseInt(argv.workers) > 0) {
+if (argv.workers && !isNaN(argv.workers) && parseInt(argv.workers) > 0) {
     workerCount = parseInt(argv.workers);
 }
 
-for(let i = 1; i < workerCount+1; i++) {
-    let process = childProcess.spawn('node', ['attack.js'], {
-    })
-    process.on('exit', () => {
+console.log(chalk.cyan('Starting workers...'));
+
+for (let i = 1; i < workerCount + 1; i++) {
+    let spawnedProcess = childProcess.spawn('node', ['attack.js'])
+    spawnedProcess.on('exit', () => {
         console.log(chalk.yellow(`Worker #${i} exited.`));
     })
-    process.on('spawn', () => {
+    spawnedProcess.on('spawn', () => {
         console.log(chalk.green(`Worker #${i} spawned.`));
     })
-    process.on('error', () => {
+    spawnedProcess.on('error', () => {
         console.log(chalk.red(`Worker #${i} encountered an error.`));
     })
-    process.stderr.on('data', (err) => {
-        console.log(chalk.bgYellow('error: ' + err));
-    })
-    process.stdout.on('data', (data) => {
-        console.log(`Hit ${data}`);
+    spawnedProcess.stdout.on('data', (data) => {
+        let site = `${data}`.trim();
+        if(counterMap.has(site)) {
+            counterMap.set(site, counterMap.get(site)+1);
+        }
+        else {
+            counterMap.set(site, 1);
+        }
+        process.stdout.cursorTo(0, i);
+        process.stdout.clearLine();
+        process.stdout.write(chalk.green(`Worker #${i}: `) + `Hit ${site}`);
     })
 }
+
+process.stdin.resume();
+
+function exitHandler() {
+    //https://stackoverflow.com/a/14032965
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0, workerCount+2);
+    counterMap.forEach((value, key) => {
+        process.stdout.write(chalk.greenBright(`Hit ${key} ${value} times.\n`));
+    })
+    process.exit();
+}
+
+process.on('exit', exitHandler.bind(null, { cleanup: true }));
+process.on('SIGINT', exitHandler.bind(null, { exit: true }));
+process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
+process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
+process.on('uncaughtException', exitHandler.bind(null, { exit: true }));
