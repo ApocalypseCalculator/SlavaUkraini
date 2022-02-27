46eaf354fb0fb8800942e961310128bb1a73dfc7
Patrick Lin <apocalypsecalculator@gmail.com>
2022-02-26 21:33:24 -0500

    fix output and add timer

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const argv = yargs(hideBin(process.argv)).argv;
 
 let workerCount = 10;
 let counterMap = new Map();
+let startTime = Date.now();
 
 if (argv.workers && !isNaN(argv.workers) && parseInt(argv.workers) > 0) {
     workerCount = parseInt(argv.workers);
@@ -46,8 +47,13 @@ process.stdin.resume();
 function exitHandler() {
     //https://stackoverflow.com/a/14032965
     process.stdout.clearLine();
-    process.stdout.cursorTo(0, workerCount + 2);
+    process.stdout.cursorTo(0, 0);
+    for(let i = 0; i < workerCount + 2; i++) {
+        process.stdout.write(' '.repeat(50) + '\n');
+    }
+    process.stdout.cursorTo(0, 0);
     process.stdout.write(chalk.bgBlue(`DDoS Report: \n`));
+    process.stdout.write(chalk.bgGray(`Time Elapsed: ${new Date(Date.now() - startTime).toISOString().slice(11, -1)}ms\n`));
     counterMap.forEach((value, key) => {
         process.stdout.write(chalk.greenBright(`Hit ${key} ${value} times.\n`));
     })
